b76fa880d201f4976557b64ac04abd2f91a13efb
Oscar Negrete <oscar.negrete09@gmail.com>
2018-11-19 21:35:49 -0500

    Routing and variables rules

--- server.py
+++ server.py
@@ -3,9 +3,20 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-
 def home():
     return "Hello World"
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+@app.route('/blog')
+def blog():
+    return 'This is the blog'
+
+@app.route('/blog/<int:blog_id>')
+def blogpost(blog_id):
+    return 'This is blog post number ' + str(blog_id)
 
 if __name__ == "__main__":
     app.run()
